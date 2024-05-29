ef6f6cb15269d86a9adde12952a5a8243ee8a032
JafarAssegaf <ajib.aiwa@gmail.com>
2024-05-29 09:42:36 +0700

    feat:init ~ Create react lesson project

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
